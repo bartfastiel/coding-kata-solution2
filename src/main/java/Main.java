d90edeb984aa7a772d0e12cb183cfb95356f5f76
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-11-20 23:49:47 +0100

    Change behavior: resulting sequence should be unique

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,16 +16,11 @@ class Main {
         }
         var value = arr[start];
 
-        // if the value is equal to the minimum, it is always a good idea to include it
-        if (value == minimum) {
-            return findLongestPossibleSequence(arr, start + 1, minimum) + 1;
-        }
-
         // what if we do not include this value?
         int lengthWithoutCurrentValue = findLongestPossibleSequence(arr, start + 1, minimum);
 
         // do we have to skip this value, because it is too small?
-        if (value < minimum) {
+        if (value <= minimum) {
             return lengthWithoutCurrentValue;
         }
 
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -21,10 +21,9 @@ class MainTest {
             "999=1",
             "1,2=2",
             "2,1=1",
-            "1,1=2",
             "1,2,1=2",
             "1,3,2,3=3",
-            "1,3,2,2,2,3=5",
+            "9,9,4,2=1",
     }, delimiter = '=')
     void check(String input, int expected) {
         int[] data;
