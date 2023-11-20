bd01831b0bcaad4a5c00813ec144bcadbbffb9ed
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-11-20 23:02:22 +0100

    Add JUnit5

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+  public static int LongestIncreasingSequence(int[] arr) {
+    // code goes here  
+    return arr[0];
+  }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void run() {
+        Main.LongestIncreasingSequence(new int[]{0});
+    }
+
+}
