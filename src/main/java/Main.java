5254f7b9c3985974013b7bb76ab2cfe9c8080c2f
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-11-20 23:05:24 +0100

    Throw expection on negatives

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
+
 class Main {
 
     public static int LongestIncreasingSequence(int[] arr) {
+        Arrays.stream(arr).forEach(i -> {
+            if (i < 0) {
+                throw new IllegalArgumentException("Only positive numbers allowed");
+            }
+        });
         return 0;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -4,6 +4,13 @@ import org.junit.jupiter.api.Test;
 class MainTest {
 
     @Test
+    void expectException_whenUsingNegatives() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Main.LongestIncreasingSequence(new int[] {-1});
+        });
+    }
+
+    @Test
     void expect0_whenUsingEmptyArray() {
         var actual = Main.LongestIncreasingSequence(new int[0]);
         Assertions.assertEquals(0, actual);
